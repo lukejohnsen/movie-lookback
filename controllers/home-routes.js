232b37333a848175d1db317fdb5177d2ce847cf2
lukejohnsen <johnsen.luke@gmail.com>
2021-12-22 20:38:27 -0700

    changing file name

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { User, Entry } = require('../models');
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
 
 router.get('/userpage', (req, res) => {
     res.render('userpage');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -13,6 +13,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // turn on routes
 app.use(routes);
